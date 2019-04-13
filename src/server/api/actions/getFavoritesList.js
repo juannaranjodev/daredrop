ffe02e1d8209ca0d27e12551ea3e43cd40aec9ec
CoolFullstackDev <topace1029@hotmail.com>
2019-04-13 13:43:17 +0300

    filter rejected and expired project from favoritesList

--- src/server/api/actions/getFavoritesList.js
+++ src/server/api/actions/getFavoritesList.js
@@ -1,10 +1,8 @@
-import { filter, propEq, split, equals } from 'ramda'
-
+import { filter, propEq, split, equals, addIndex, map, nth, head, last } from 'ramda'
+const R = require('ramda')
 import { TABLE_NAME, documentClient } from 'root/src/server/api/dynamoClient'
-import { GSI1_INDEX_NAME, GSI1_PARTITION_KEY } from 'root/src/shared/constants/apiDynamoIndexes'
-
+import { GSI1_INDEX_NAME, GSI1_PARTITION_KEY, PARTITION_KEY, SORT_KEY } from 'root/src/shared/constants/apiDynamoIndexes'
 import { dynamoItemsProp, pkProp, skProp, projectApprovedKey } from 'root/src/server/api/lenses'
-import dynamoQueryProject from 'root/src/server/api/actionUtil/dynamoQueryProject'
 import listResults from 'root/src/server/api/actionUtil/listResults'
 import favoritesSerializer from 'root/src/server/api/serializers/favoritesSerializer'
 import moment from 'moment'
@@ -27,18 +25,43 @@ export default async ({ userId }) => {
     const favoritesProjects = filter(propEq('favoritesAmount', 1), dynamoItemsProp(dynamoResults))
 
     // check if each project is not rejected or expired
-    const filterValidate = async dare => {
-        const [project] = await dynamoQueryProject(
-            userId, pkProp(dare),
-        )
+    const filterValidate = async (dare) => {
+        const projectParams = {
+            TableName: TABLE_NAME,
+            KeyConditionExpression: `${PARTITION_KEY} = :pk and begins_with(${SORT_KEY}, :project)`,
+            ExpressionAttributeValues: {
+                ':pk': pkProp(dare),
+                ':project': 'project',
+            },
+            ConsistentRead: true,
+        }
 
-        const diff = moment().diff(project.approved, 'days')
-        const [, status] = split('|', skProp(project))
+        const projectDdb = await documentClient.query(
+            projectParams,
+        ).promise()
+
+        const project = dynamoItemsProp(projectDdb)
+
+        const diff = moment().diff(head(project).approved, 'days')
+        const [, status] = split('|', head(project).sk)
 
         return equals(status, projectApprovedKey) && diff <= 30
+
     }
 
-    const availableFavoritesList = filter(filterValidate, favoritesProjects)
+    const availableArray = await Promise.all(
+        map(filterValidate, favoritesProjects)
+    )
+
+    const mapIndexed = addIndex(map);
+    const availableIndexArray = mapIndexed((val, idx) => val ? idx : 99999, availableArray);
+
+    const resultArray = map(index => index == 99999 ? null : nth(index, favoritesProjects), availableIndexArray)
+
+    const availableFavoritesList = resultArray.filter(function (x) {
+        return (x !== null);
+    });
+
 
     return listResults({
         dynamoResults: { Items: availableFavoritesList },
