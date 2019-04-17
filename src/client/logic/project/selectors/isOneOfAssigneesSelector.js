00580ba455cd5d2aa28635ad8872dd1c8f2deb44
AnnaBova <anna.bova23@gmail.com>
2019-04-17 12:15:00 +0300

    Delivery dare button

--- src/client/logic/project/selectors/isOneOfAssigneesSelector.js
+++ src/client/logic/project/selectors/isOneOfAssigneesSelector.js
@@ -1,8 +1,10 @@
-import { length, map, prop, intersection } from 'ramda'
-import objectToArray from 'root/src/client/logic/api/util/objectToArray'
+import projectAssigneesSelector from 'root/src/client/logic/project/selectors/projectAssigneesSelector'
+import getUserDataSelector from 'root/src/client/logic/api/selectors/getUserDataSelector'
 
-export default (assignees, userData) => {
-	const userDataDisplayNames = map(prop('displayName'), objectToArray(userData))
-	const assigneesDisplayNames = map(prop('displayName'), assignees)
-	return length(intersection(userDataDisplayNames, assigneesDisplayNames))
+export default (state, props) => {
+	const assignees = projectAssigneesSelector(state, props)
+	const userData = getUserDataSelector(state, props)
+
+	return assignees
+		.filter(assignee => assignee.username === userData.displayName).length > 0
 }
