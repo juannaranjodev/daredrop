4d4696a1f92f1ad529b51c39377f6509c5187163
AnnaBova <anna.bova23@gmail.com>
2019-04-19 17:05:49 +0300

    fix twich auth problem

--- src/client/logic/project/selectors/isOneOfAssigneesSelector.js
+++ src/client/logic/project/selectors/isOneOfAssigneesSelector.js
@@ -1,10 +1,13 @@
+import { length, map, prop, intersection } from 'ramda'
+import objectToArray from 'root/src/client/logic/api/util/objectToArray'
+
 import projectAssigneesSelector from 'root/src/client/logic/project/selectors/projectAssigneesSelector'
 import getUserDataSelector from 'root/src/client/logic/api/selectors/getUserDataSelector'
 
 export default (state, props) => {
 	const assignees = projectAssigneesSelector(state, props)
 	const userData = getUserDataSelector(state, props)
-
-	return assignees
-		.filter(assignee => assignee.username === userData.displayName).length > 0
+	const userDataDisplayNames = map(prop('displayName'), objectToArray(userData))
+	const assigneesDisplayNames = map(prop('displayName'), assignees)
+	return length(intersection(userDataDisplayNames, assigneesDisplayNames))
 }
