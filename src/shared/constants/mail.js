b072852d091cd680ad9f11c13d3e9f6df024cfbf
Dominik Piekarski <piekarski_d@o2.pl>
2019-04-21 19:27:18 +0200

    switched some constants

--- src/shared/constants/mail.js
+++ src/shared/constants/mail.js
@@ -1,9 +1,7 @@
-export const logoSrc = 'https://cdn2.bigcommerce.com/server1500/ac84d/products/1204/images/2688/Adidas_Logo_Flower__83153.1337144903.1280.1280.jpg?c=2'
+export const ourUrl = 'dev.watt.tv'
+export const logoSrc = `${ourUrl}/logo.png`
+export const rulesOfUseUrl = `${ourUrl}/terms`
+export const marketplaceUrl = `${ourUrl}/marketplace`
 export const survey = 'google.com/survey'
 export const ourName = 'Dare Drop'
 export const ourEmail = 'piekarski_d@o2.pl'
-// export const ourUrl = 'DoubleDog.tv'
-// for testing purposes localhost
-export const ourUrl = 'localhost:8585'
-export const rulesOfUseLink = 'google.com'
-export const marketplaceUrl = 'google.com/survey'
