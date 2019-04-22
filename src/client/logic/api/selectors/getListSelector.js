b7d3d2536a760f2c29de2ccaffca87fe6d27df44
AnnaBova <anna.bova23@gmail.com>
2019-04-22 11:51:17 +0300

    Fix style  and logic

--- src/client/logic/api/selectors/getListSelector.js
+++ src/client/logic/api/selectors/getListSelector.js
@@ -8,20 +8,26 @@ export default (state, { moduleId }) => {
 	const endpointId = moduleEndpointIdSelector(state, { moduleId })
 	const listProcessing = Object.keys(viewListProcessing(state) || [])
 	let moduleProcess
+	let list = []
 	if (typeof endpointId === 'string') {
 		moduleProcess = filter(process => contains(endpointId, process), listProcessing)
+		list = viewItems(moduleProcess[0], state)
 	} else {
 		moduleProcess = reduce(
 			(acc, endpoint) => acc.concat(filter(process => contains(endpoint, process), listProcessing)),
 			[],
 			endpointId,
 		)
-	}
 
-	const list = map((item) => {
-		const result = viewItems(item, state)
-		return !isNil(result) ? result : []
-	}, moduleProcess)
+		list = map((item) => {
+			const result = viewItems(item, state)
+			return !isNil(result) ? result : []
+		}, moduleProcess)
+		const filterArr = list[list.length - 1]
+		list = list.slice(0, list.length - 1)
+		list = reduce((acc, item) => acc.concat(item), [], list)
+		list = [list, filterArr]
+	}
 
 	return list
 }
