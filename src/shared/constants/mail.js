450086a626a489f89a24052c32e669427eee5dc5
jbechtold <joebechtold@gmail.com>
2019-04-24 08:18:05 -0400

    updated links

--- src/shared/constants/mail.js
+++ src/shared/constants/mail.js
@@ -1,7 +1,7 @@
 export const ourUrl = process.env.RECORD_SET || 'localhost:8585'
 export const logoSrc = `${ourUrl}/logo.png`
-export const rulesOfUseUrl = `${ourUrl}/terms`
+export const rulesOfUseUrl = `${ourUrl}/rules-of-use`
 export const marketplaceUrl = `${ourUrl}/marketplace`
-export const survey = 'google.com/survey'
+export const survey = 'forms.gle/iqnCmQurEtNRK8Xd8'
 export const ourName = 'Dare Drop'
-export const ourEmail = 'piekarski_d@o2.pl'
+export const ourEmail = 'support@daredrop.com'
