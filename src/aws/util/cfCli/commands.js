2b2588565166daab470983ee7bc75a1f001991e6
Dominik Piekarski <piekarski_d@o2.pl>
2019-04-27 23:01:00 +0200

    added commands to pkg.json for running local lambda with docker

--- src/aws/util/cfCli/commands.js
+++ src/aws/util/cfCli/commands.js
@@ -26,6 +26,9 @@ const afterCreateOrUpdate = [
 	{ title: 'Saving stack output', fn: saveStackOutputs },
 	{ title: 'Uploading statics', fn: uploadStatics },
 ]
+const bundleLambda = [
+	{ title: 'Bundling lambda functions', fn: webpackLambda }
+]
 
 const createUpdateCommon = [
 	{ title: 'Bundling lambda functions', fn: webpackLambda },
@@ -58,5 +61,5 @@ const outputs = [
 ]
 
 export default {
-	create, update, remove, outputs,
+	create, update, remove, outputs, bundleLambda
 }
