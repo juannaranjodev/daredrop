12944530875814ba59c000aa4b26d1e5b3719592
AnnaBova <anna.bova23@gmail.com>
2019-05-03 12:44:29 +0300

    fix implementation of acceptance of project

--- src/server/api/actions/getAcceptProject.js
+++ src/server/api/actions/getAcceptProject.js
@@ -1,15 +1,16 @@
 import { TABLE_NAME, documentClient } from 'root/src/server/api/dynamoClient'
 import {
-	GSI1_INDEX_NAME, GSI1_PARTITION_KEY,
+	GSI1_INDEX_NAME, SORT_KEY
 } from 'root/src/shared/constants/apiDynamoIndexes'
 import { projectAcceptedKey } from 'root/src/server/api/lenses'
+
 
 
 export default async () => {
 	const acceptedProjectParams = {
 		TableName: TABLE_NAME,
 		IndexName: GSI1_INDEX_NAME,
-		KeyConditionExpression: `${GSI1_PARTITION_KEY} = :accepted`,
+		KeyConditionExpression: `begins_with(${SORT_KEY}, :accepted)`,
 		ExpressionAttributeValues: {
 			':accepted': `project|${projectAcceptedKey}`,
 		},
