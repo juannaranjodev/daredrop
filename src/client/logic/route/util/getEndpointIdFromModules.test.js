47330a90452e5de79b908c60d490d8aca3756de4
AnnaBova <anna.bova23@gmail.com>
2019-05-03 14:32:26 +0300

    fix test

--- src/client/logic/route/util/getEndpointIdFromModules.test.js
+++ src/client/logic/route/util/getEndpointIdFromModules.test.js
@@ -11,13 +11,14 @@ import {
 	GET_FAVORITES_LIST,
 	GET_MY_PROJECTS,
 	GET_PLEDGED_PROJECTS,
+	GET_ACCEPTED_PROJECTS,
 } from 'root/src/shared/descriptions/endpoints/endpointIds'
 
 test('returns correct endpointId from module', () => {
 	// currently if this test fails look into implementation of root/src/client/logic/route/util/getCurrentModuleId.js
 	// which takes last element of an array and sets it as an endpoint id for current module
 	expect(getEndpointIdFromModules(PENDING_PROJECTS_LIST_MODULE_ID)).toEqual([GET_PENDING_PROJECTS, GET_PLEDGED_PROJECTS])
-	expect(getEndpointIdFromModules(ACTIVE_PROJECTS_LIST_MODULE_ID)).toEqual([GET_ACTIVE_PROJECTS, GET_PLEDGED_PROJECTS])
+	expect(getEndpointIdFromModules(ACTIVE_PROJECTS_LIST_MODULE_ID)).toEqual([GET_ACTIVE_PROJECTS, GET_PLEDGED_PROJECTS, GET_ACCEPTED_PROJECTS])
 	expect(getEndpointIdFromModules(FAVORITES_PROJECTS_LIST_MODULE_ID)).toEqual([GET_FAVORITES_LIST, GET_PLEDGED_PROJECTS])
 	expect(getEndpointIdFromModules(MY_PROJECTS_LIST_MODULE_ID)).toEqual([GET_MY_PROJECTS, GET_PLEDGED_PROJECTS])
 })
