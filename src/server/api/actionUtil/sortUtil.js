4f41e68d9c4acf4e5dc14cfc44db9db8e0759f6b
Sebastian Corradini <sebastian34237@gmail.com>
2019-05-05 13:46:51 -0300

    Finished Order marketplace by approved Finished Sorting before pagenation

--- src/server/api/actionUtil/sortUtil.js
+++ src/server/api/actionUtil/sortUtil.js
@@ -2,3 +2,5 @@ import { ascend, descend, prop } from 'ramda'
 
 export const ascendingCreated = ascend(prop('created'))
 export const descendingCreated = descend(prop('created'))
+export const ascendingApproved = ascend(prop('approved'))
+export const descendingApproved = descend(prop('approved'))
