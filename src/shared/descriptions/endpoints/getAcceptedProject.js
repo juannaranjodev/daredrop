879377bba3222934975c7806005216baa0a60892
AnnaBova <anna.bova23@gmail.com>
2019-05-06 11:11:35 +0300

    fix recordTypes

--- src/shared/descriptions/endpoints/getAcceptedProject.js
+++ src/shared/descriptions/endpoints/getAcceptedProject.js
@@ -1,10 +1,10 @@
 import { GET_ACCEPTED_PROJECTS } from 'root/src/shared/descriptions/endpoints/endpointIds'
 import { listEndpointType } from 'root/src/shared/descriptions/endpoints/lenses'
-import { project } from 'root/src/shared/descriptions/endpoints/recordTypes'
+import { projectAccepted } from 'root/src/shared/descriptions/endpoints/recordTypes'
 
 export default {
 	[GET_ACCEPTED_PROJECTS]: {
 		endpointType: listEndpointType,
-		recordType: project,
+		recordType: projectAccepted,
 	},
 }
