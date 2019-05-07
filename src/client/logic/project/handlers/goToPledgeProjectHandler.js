ed56f90cd884ac8f1b0a4e129486fa7a30fa4d50
AnnaBova <anna.bova23@gmail.com>
2019-05-07 09:22:26 +0300

    Back link

--- src/client/logic/project/handlers/goToPledgeProjectHandler.js
+++ src/client/logic/project/handlers/goToPledgeProjectHandler.js
@@ -2,6 +2,6 @@ import {
 	PLEDGE_PROJECT_ROUTE_ID,
 } from 'root/src/shared/descriptions/routes/routeIds'
 
-export default (recordId, pushRoute) => () => (
-	pushRoute(PLEDGE_PROJECT_ROUTE_ID, { recordId })
+export default (payload, pushRoute) => () => (
+	pushRoute(PLEDGE_PROJECT_ROUTE_ID, payload)
 )
