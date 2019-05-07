25366918ed82fdcab8d04545584765fa2955be47
AnnaBova <anna.bova23@gmail.com>
2019-05-07 10:48:01 +0300

    fix type error

--- src/client/logic/static/selectors/sharedUrlSelector.js
+++ src/client/logic/static/selectors/sharedUrlSelector.js
@@ -1,6 +1,7 @@
 import urlFromRouteObj from 'root/src/client/logic/route/util/urlFromRouteObj'
 import currentRouteParams from 'root/src/client/logic/route/selectors/currentRouteParams'
 import origin from 'root/src/shared/constants/origin'
+import { isNil } from 'ramda'
 
 import {
 	VIEW_PROJECT_ROUTE_ID,
@@ -8,10 +9,11 @@ import {
 
 export default (state, props) => {
 	const routeParams = currentRouteParams(state, props)
-	if (routeParams) {
+
+	if (!isNil(routeParams)) {
 		const localUrl = urlFromRouteObj({
 			routeId: VIEW_PROJECT_ROUTE_ID,
-			routeParams: { recordId: String(routeParams.recordId) },
+			routeParams: { recordId: routeParams.recordId },
 		})
 
 		return `${origin}${localUrl}`
