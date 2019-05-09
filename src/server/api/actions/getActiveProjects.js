2ba6d30ed42a90b830c5653ff7043863e1b3ea3e
Sebastian Corradini <sebastian34237@gmail.com>
2019-05-09 17:00:37 -0300

    Removed unused code

--- src/server/api/actions/getActiveProjects.js
+++ src/server/api/actions/getActiveProjects.js
@@ -2,10 +2,9 @@ import getProjectsByStatus from 'root/src/server/api/actionUtil/getProjectsBySta
 import { projectApprovedKey } from 'root/src/server/api/lenses'
 import { descendingApproved } from 'root/src/server/api/actionUtil/sortUtil'
 import getFilteredProjects from 'root/src/server/api/actions/getFilteredProjects'
-// export default async payload => getProjectsByStatus(projectApprovedKey, descendingApproved, payload)
-// 	.then(projects => projects)
+
 export default async (payload) =>{
 	const filteredProjects = await getFilteredProjects(payload)
-	const projects = await getProjectsByStatus(projectApprovedKey, descendingApproved, payload, filteredProjects)
-	return projects
+
+	return await getProjectsByStatus(projectApprovedKey, descendingApproved, payload, filteredProjects)
 }
