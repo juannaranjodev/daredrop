4aaadc741efded0830493d344701d9405223ed3f
Sebastian Corradini <sebastian34237@gmail.com>
2019-05-09 14:06:23 -0300

    Finished getFilteredProjectsByGame

--- src/server/api/actions/getFilteredProjectsByGame.js
+++ src/server/api/actions/getFilteredProjectsByGame.js
@@ -4,7 +4,7 @@ import {
 	GSI1_INDEX_NAME, GSI1_PARTITION_KEY,
 } from 'root/src/shared/constants/apiDynamoIndexes'
 import listResults from 'root/src/server/api/actionUtil/listResults'
-import filteredProjectSerializer from 'root/src/server/api/serializers/pledgeSerializer'
+import filteredProjectSerializer from 'root/src/server/api/serializers/filteredProjectSerializer'
 
 export default async ({ payload }) => {
 	const filteredProjectIdParams = {
