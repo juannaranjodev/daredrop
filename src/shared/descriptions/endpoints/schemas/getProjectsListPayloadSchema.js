a072cb199b7787ed1a53c9fa3cdc6678dbd51ab3
Sebastian Corradini <sebastian34237@gmail.com>
2019-05-09 16:29:45 -0300

    Finished getActiveProject with filter.

--- src/shared/descriptions/endpoints/schemas/getProjectsListPayloadSchema.js
+++ src/shared/descriptions/endpoints/schemas/getProjectsListPayloadSchema.js
@@ -2,6 +2,8 @@ export default {
 	type: 'object',
 	properties: {
 		currentPage: { type: 'integer' },
+		gameId: { type: 'string' },
+		streamerId: { type: 'string' },
 	},
 	required: ['currentPage'],
 	additionalProperties: false,
