7fc6cde0e54769359bf19731bdf1a0f529dcddb9
Sebastian Corradini <sebastian34237@gmail.com>
2019-05-10 19:36:12 -0300

    Updated for default sortType

--- src/server/api/actions/getActiveProjects.js
+++ src/server/api/actions/getActiveProjects.js
@@ -1,5 +1,5 @@
 import getProjectsByStatus from 'root/src/server/api/actionUtil/getProjectsByStatus'
 import { projectApprovedKey } from 'root/src/server/api/lenses'
-
-export default async payload => getProjectsByStatus(projectApprovedKey, payload)
+import {SORT_BY_NEWEST} from 'root/src/shared/constants/sortTypesOfProject'
+export default async payload => getProjectsByStatus(projectApprovedKey, SORT_BY_NEWEST , payload)
 	.then(projects => projects)
