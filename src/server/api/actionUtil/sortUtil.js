d3ad43c2885f1836d8365a632ff74483c7186d7e
Sebastian Corradini <sebastian34237@gmail.com>
2019-05-10 05:50:22 -0300

    Added const for sort type Added sort functions by sort type Updated getAcceptProject for accepted date

--- src/server/api/actionUtil/sortUtil.js
+++ src/server/api/actionUtil/sortUtil.js
@@ -1,6 +1,10 @@
 import { ascend, descend, prop } from 'ramda'
+import {SORT_BY_BOUNTY,SORT_BY_TIME_LEFT,SORT_BY_NEWEST,SORT_BY_ACCEPTED} from 'root/src/shared/constants/sortTypesOfProject'
 
 export const ascendingCreated = ascend(prop('created'))
 export const descendingCreated = descend(prop('created'))
 export const ascendingApproved = ascend(prop('approved'))
 export const descendingApproved = descend(prop('approved'))
+export const sortByType = {
+    [SORT_BY_BOUNTY] : descend(prop('pledgeAmount')),
+}
