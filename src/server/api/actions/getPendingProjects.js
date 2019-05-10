671077b68f19d63fe9ab7e1b63158c9fdf99a3fc
Sebastian Corradini <sebastian34237@gmail.com>
2019-05-10 19:13:25 -0300

    Updated getPendingProjects for new getProjectByStatus

--- src/server/api/actions/getPendingProjects.js
+++ src/server/api/actions/getPendingProjects.js
@@ -1,6 +1,6 @@
 import getProjectsByStatus from 'root/src/server/api/actionUtil/getProjectsByStatus'
 import { projectPendingKey } from 'root/src/server/api/lenses'
-import {SORT_BY_CREATED_ASC} from 'root/src/shared/constants/sortTypesOfProject'
+// import {SORT_BY_CREATED_ASC} from 'root/src/shared/constants/sortTypesOfProject'
 
-export default async payload => getProjectsByStatus(projectPendingKey,SORT_BY_CREATED_ASC, payload)
+export default async payload => getProjectsByStatus(projectPendingKey, payload)
 	.then(projects => projects)
