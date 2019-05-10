02f072c7ed77b3abdc699dfebfe27500fbabcf78
Sebastian Corradini <sebastian34237@gmail.com>
2019-05-10 19:13:00 -0300

    Updated getActiveProject for new getProjectsByStatus

--- src/server/api/actions/getActiveProjects.js
+++ src/server/api/actions/getActiveProjects.js
@@ -1,13 +1,5 @@
-import {prop,sort,descend,ascend} from 'ramda'
 import getProjectsByStatus from 'root/src/server/api/actionUtil/getProjectsByStatus'
 import { projectApprovedKey } from 'root/src/server/api/lenses'
-import getFilteredProjects from 'root/src/server/api/actions/getFilteredProjects'
 
-import {SORT_BY_NEWEST} from 'root/src/shared/constants/sortTypesOfProject'
-
-export default async (payload) =>{
-	const filteredProjects = await getFilteredProjects(payload)
-	let sortType = prop("sortType",payload.payload)
-	sortType = sortType ? sortType : SORT_BY_NEWEST
-	return await getProjectsByStatus(projectApprovedKey, sortType, payload, filteredProjects)
-}
+export default async payload => getProjectsByStatus(projectApprovedKey, payload)
+	.then(projects => projects)
