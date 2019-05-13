63361cb63e81a0d8a087fac9e5cf7acf4d011ee9
Sebastian Corradini <sebastian34237@gmail.com>
2019-05-13 14:04:37 -0300

    Updated logic for to forEach

--- src/server/api/actionUtil/getFilteredProjectIds.js
+++ src/server/api/actionUtil/getFilteredProjectIds.js
@@ -1,41 +1,33 @@
-import { intersection, reduce,prop } from 'ramda'
+import { intersection, prop, map } from 'ramda'
 import { TABLE_NAME, documentClient } from 'root/src/server/api/dynamoClient'
 
 import {
     GSI1_INDEX_NAME, GSI1_PARTITION_KEY,
 } from 'root/src/shared/constants/apiDynamoIndexes'
-import listResults from 'root/src/server/api/actionUtil/listResults'
-import filteredProjectSerializer from 'root/src/server/api/serializers/filteredProjectSerializer'
+import { dynamoItemsProp } from 'root/src/server/api/lenses'
 
 export default async (items) => {
-
-    if (items == undefined || items.length == 0) return null
-    const filterByParam = async (param, value) => {
-        const filteredProjectIdParams = {
-            TableName: TABLE_NAME,
-            IndexName: GSI1_INDEX_NAME,
-            KeyConditionExpression: `${GSI1_PARTITION_KEY} = :pk`,
-            ExpressionAttributeValues: {
-                ':pk': `${param}|${value}`,
-            },
-        }
-        const dynamoResults = await documentClient.query(
-            filteredProjectIdParams,
-        ).promise()
-        return prop("items",listResults({
-            dynamoResults,
-            serializer: filteredProjectSerializer,
-        }))
-    }
+    const filteredResults = await Promise.all(
+        map((item) => {return documentClient.query({
+                TableName: TABLE_NAME,
+                IndexName: GSI1_INDEX_NAME,
+                KeyConditionExpression: `${GSI1_PARTITION_KEY} = :pk`,
+                ExpressionAttributeValues: {
+                    ':pk': `${item.param}|${item.value}`,
+                }
+            }
+            ).promise()},items)
+    )
 
     let result = null
-    for (const item of items) {
+
+    filteredResults.forEach((filteredResult)=>{
         if (result == null) {
-            result = await filterByParam(item.param, item.value)
+            result = map((item) => {return {"id":prop('pk',item)}}, dynamoItemsProp(filteredResult))
         }
         else {
-            result = intersection(result, await filterByParam(item.param, item.value))
+            result = intersection(result, map(prop('pk'), dynamoItemsProp(filteredResult)))
         }
-    }
+    })
     return result
 }
