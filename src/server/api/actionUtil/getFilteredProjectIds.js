4cf410333f06090a94ed0c8b860ccde16379972b
Sebastian Corradini <sebastian34237@gmail.com>
2019-05-14 07:39:48 -0300

    Updated some files for readability

--- src/server/api/actionUtil/getFilteredProjectIds.js
+++ src/server/api/actionUtil/getFilteredProjectIds.js
@@ -1,4 +1,4 @@
-import { intersection, prop, map } from 'ramda'
+import { intersection, prop, map, forEach } from 'ramda'
 import { TABLE_NAME, documentClient } from 'root/src/server/api/dynamoClient'
 
 import {
@@ -7,9 +7,12 @@ import {
 import { dynamoItemsProp } from 'root/src/server/api/lenses'
 
 export default async (items) => {
-    if (items == undefined || items.length == 0) return null
+    if (items == undefined || items.length == 0) {
+        return null
+    }
     const filteredResults = await Promise.all(
-        map((item) => {return documentClient.query({
+        map((item) => {
+            return documentClient.query({
                 TableName: TABLE_NAME,
                 IndexName: GSI1_INDEX_NAME,
                 KeyConditionExpression: `${GSI1_PARTITION_KEY} = :pk`,
@@ -17,18 +20,28 @@ export default async (items) => {
                     ':pk': `${item.param}|${item.value}`,
                 }
             }
-            ).promise()},items)
+            ).promise()
+        }, items)
     )
 
     let result = null
 
-    filteredResults.forEach((filteredResult)=>{
+    forEach((filteredResult) => {
         if (result == null) {
-            result = map((item) => {return {"id":prop('pk',item)}}, dynamoItemsProp(filteredResult))
+            result = map((item) => {
+                return {
+                    "id": prop('pk', item)
+                }
+            }, dynamoItemsProp(filteredResult))
         }
         else {
-            result = intersection(result, map((item) => {return {"id":prop('pk',item)}}, dynamoItemsProp(filteredResult)))
+            result = intersection(result,
+                map((item) => {
+                    return {
+                        "id": prop('pk', item)
+                    }
+                },dynamoItemsProp(filteredResult)))
         }
-    })
+    }, filteredResults)
     return result
 }
