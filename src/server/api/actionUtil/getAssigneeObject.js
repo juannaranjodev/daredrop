cff6a09ad9eb44f50e17cc0214537cf47356b0d5
AnnaBova <anna.bova23@gmail.com>
2019-05-17 09:02:27 +0300

    fix native function

--- src/server/api/actionUtil/getAssigneeObject.js
+++ src/server/api/actionUtil/getAssigneeObject.js
@@ -1,10 +1,10 @@
-import { dissoc, assoc, compose, map } from 'ramda'
+import { dissoc, assoc, compose, map, split, prop } from 'ramda'
 import { SORT_KEY } from 'root/src/shared/constants/apiDynamoIndexes'
 
 export default assignees => map(assignee => compose(
 	dissoc('modified'),
 	dissoc('sk'),
 	dissoc('pk'),
-	assoc('platform', assignee[SORT_KEY].split('|')[1]),
-	assoc('platformId', assignee[SORT_KEY].split('|')[2]),
+	assoc('platform', prop(1, split('|', assignee[SORT_KEY]))),
+	assoc('platformId', prop(2, split('|', assignee[SORT_KEY]))),
 )(assignee), assignees)
