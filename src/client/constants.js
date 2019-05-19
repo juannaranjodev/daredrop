b35d562b7b9edb23e889c9a22b4f529578650f02
Dominik Piekarski <piekarski_d@o2.pl>
2019-05-19 18:46:40 +0200

    added refresh token support

--- src/client/constants.js
+++ src/client/constants.js
@@ -5,6 +5,7 @@ const twitchUrl = () => {
 	const responseType = 'token'
 	const redirectUri = `http://${window.location.host}/twitch-oauth`
 	const scopes = buildTwitchScopes(twitchScopes)
+	const grantType = 'refresh_token'
 	let clientId
 
 	if (window.location.host === 'localhost:8585') {
@@ -12,7 +13,7 @@ const twitchUrl = () => {
 	} else {
 		clientId = TWITCH_CLIENT_ID
 	}
-	return `https://id.twitch.tv/oauth2/authorize?client_id=${clientId}&response_type=${responseType}&redirect_uri=${redirectUri}${scopes}`
+	return `https://id.twitch.tv/oauth2/authorize?grant_type=${grantType}&client_id=${clientId}&response_type=${responseType}&redirect_uri=${redirectUri}${scopes}`
 }
 
 export const twitchOauthUrl = twitchUrl()
