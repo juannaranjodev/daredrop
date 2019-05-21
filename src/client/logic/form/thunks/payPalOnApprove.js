7bd8491388b8cbfa499959e3f9ab72e15c00229e
Dominik Piekarski <piekarski_d@o2.pl>
2019-05-22 00:16:03 +0200

    handled error

--- src/client/logic/form/thunks/payPalOnApprove.js
+++ src/client/logic/form/thunks/payPalOnApprove.js
@@ -5,14 +5,11 @@ import successPageSelector from 'root/src/client/logic/project/selectors/success
 import pushRoute from 'root/src/client/logic/route/thunks/pushRoute'
 
 export default (data, actions, { moduleId, formSchema, formData, moduleKey }) => async (dispatch) => {
-	try {
-		actions.order.authorize().then((details) => {
-			console.log('Authorize data below')
-			console.log(details)
-			const successPage = successPageSelector(moduleId)
-			dispatch(pushRoute(successPage))
-		})
-	} catch (err) {
-		dispatch(setFormErrors(moduleKey, err))
-	}
+	actions.order.authorize().then((details) => {
+		console.log('Authorize data below')
+		console.log(details)
+		const routeId = successPageSelector(moduleId)
+		dispatch(pushRoute(routeId))
+		return 'asdsadsad'
+	}).catch(err => dispatch(setFormErrors(moduleKey, err)))
 }
