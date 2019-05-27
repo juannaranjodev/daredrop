e0d0788bb205d64cc2f3d96abd6bb2544907d9c3
itsolution-git <itsolution0820@gmail.com>
2019-05-27 14:10:02 -0400

    update bounty amount for progress bar

--- src/client/logic/project/selectors/goalProgressSelector.js
+++ src/client/logic/project/selectors/goalProgressSelector.js
@@ -1,38 +1,42 @@
 import getRecordSelector from 'root/src/client/logic/api/selectors/getRecordSelector'
+import { ternary } from 'root/src/shared/util/ramdaPlus'
 import { reduce, isNil, equals, propOr, prop } from 'ramda'
 import { GET_PROJECT } from 'root/src/shared/descriptions/endpoints/endpointIds'
 import { getResponseLenses } from 'root/src/server/api/getEndpointDesc'
 import {
-	projectAcceptedKey,
+  projectAcceptedKey,
 } from 'root/src/server/api/lenses'
 
 const responseLenses = getResponseLenses(GET_PROJECT)
 const {
-	viewId,
-	viewAmountRequested,
-	viewPledgeAmount,
-	viewAssignees,
-	viewStatus,
+  viewId,
+  viewAmountRequested,
+  viewPledgeAmount,
+  viewAssignees,
+  viewStatus,
 } = responseLenses
 
 export default (state, props) => {
-	const record = getRecordSelector(state, props)
-	const accepted = equals(projectAcceptedKey, viewStatus(record))
+  const record = getRecordSelector(state, props)
+  const accepted = equals(projectAcceptedKey, viewStatus(record))
 
-	if (accepted) {
-		const assignees = viewAssignees(record)
-		const pledgeRequest = reduce(
-			(accum, assignee) => {
-				const amountRequest = prop('amountRequested', assignee)
-				return isNil(amountRequest) ? accum + 0 : accum + amountRequest
-			},
-			0,
-			assignees,
-		)
-		const pledgeAmount = prop('pledgeAmount', record)
-		const onePersent = pledgeRequest / 95
-		const answer = (pledgeAmount / onePersent) + 5
-		return answer > 100 ? 100 : answer
-	}
-	return 5
+  if (accepted) {
+    const assignees = viewAssignees(record)
+    const pledgeRequest = reduce(
+      (accum, assignee) => {
+        const amountRequest = prop('amountRequested', assignee)
+        return isNil(amountRequest) ? accum + 0 : accum + amountRequest
+      },
+      0,
+      assignees,
+    )
+    if (equals(pledgeRequest, 0)) {
+      return 5
+    } else {
+      const pledgeAmount = prop('pledgeAmount', record)
+      const answer = pledgeAmount / pledgeRequest * 100
+      return answer < 5 ? 5 : answer > 100 ? 100 : answer
+    }
+  }
+  return 5
 }
