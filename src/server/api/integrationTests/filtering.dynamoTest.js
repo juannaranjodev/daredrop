b9e6c03e51f3afa810c025fa88fb3f306350c63b
Dominik Piekarski <piekarski_d@o2.pl>
2019-05-30 11:02:58 +0200

    added corner case

--- src/server/api/integrationTests/filtering.dynamoTest.js
+++ src/server/api/integrationTests/filtering.dynamoTest.js
@@ -20,79 +20,93 @@ import reviewDelivery from 'root/src/server/api/actions/reviewDelivery'
 import getMyProjects from 'root/src/server/api/actions/getMyProjects'
 
 describe('approveDelivery', async () => {
- let project
- test('pending delivery is displayed on review page, but is not on marketplace', async () => {
-  project = await createProject({
-   userId: 'user-differentuserid',
-   payload: createProjectPayload(),
-  })
-  await auditProject({
-   userId: mockUserId,
-   payload: {
-    projectId: project.id,
-    audit: projectApprovedKey,
-   },
-  })
-  const oAuthDetails = {
-   tokenId: 'twitch',
-   id: project.assignees[0].platformId,
-  }
-  await addOAuthToken({
-   payload: oAuthDetails,
-   userId: mockUserId,
-  })
-  await acceptProject({
-   userId: mockUserId,
-   payload: {
-    projectId: project.id,
-    assigneeId: `twitch|${project.assignees[0].platformId}`,
-    amountRequested: 1000,
-   },
-  })
-  const deliveryPayload = deliveryDareMock(project.id)
-  const deliveryInit = await deliveryDareInit({
-   userId: mockUserId,
-   payload: deliveryPayload,
-  })
-  const { deliverySortKey } = deliveryInit
-  const deliveryFinishPayload = {
-   projectId: project.id,
-   deliverySortKey,
-  }
-  await deliveryDare({
-   userId: mockUserId,
-   payload: deliveryFinishPayload,
-  })
-  const projectsOnMarketplace = await getActiveProjects({ payload: {} })
+	let project
+	test('pending delivery is displayed on review page, but is not on marketplace', async () => {
+		project = await createProject({
+			userId: 'user-differentuserid',
+			payload: createProjectPayload(),
+		})
+		await auditProject({
+			userId: mockUserId,
+			payload: {
+				projectId: project.id,
+				audit: projectApprovedKey,
+			},
+		})
+		const oAuthDetails = {
+			tokenId: 'twitch',
+			id: project.assignees[0].platformId,
+		}
+		await addOAuthToken({
+			payload: oAuthDetails,
+			userId: mockUserId,
+		})
+		await acceptProject({
+			userId: mockUserId,
+			payload: {
+				projectId: project.id,
+				assigneeId: `twitch|${project.assignees[0].platformId}`,
+				amountRequested: 1000,
+			},
+		})
+		const deliveryPayload = deliveryDareMock(project.id)
+		const deliveryInit = await deliveryDareInit({
+			userId: mockUserId,
+			payload: deliveryPayload,
+		})
+		const { deliverySortKey } = deliveryInit
+		const deliveryFinishPayload = {
+			projectId: project.id,
+			deliverySortKey,
+		}
+		await deliveryDare({
+			userId: mockUserId,
+			payload: deliveryFinishPayload,
+		})
+		const projectsOnMarketplace = await getActiveProjects({ payload: {} })
 
-  const pendingDeliveries = await getPendingDeliveries({ payload: {} })
+		const pendingDeliveries = await getPendingDeliveries({ payload: {} })
 
-  expect(pendingDeliveries.items[0].deliveries.length).toEqual(1)
-  expect(projectsOnMarketplace.items[0]).toEqual(omit(['deliveries'], pendingDeliveries.items[0]))
- })
- test('doesn\'t displays expired projects but deliveries are shown', async () => {
-  const inTwoWeeks = moment().add(14, 'days').format()
-  mockdate.set(inTwoWeeks)
-  const projectsOnMarketplace = await getActiveProjects({ payload: {} })
-  const pendingDeliveries = await getPendingDeliveries({ payload: {} })
+		expect(pendingDeliveries.items[0].deliveries.length).toEqual(1)
+		expect(projectsOnMarketplace.items[0]).toEqual(omit(['deliveries'], pendingDeliveries.items[0]))
+	})
+	test('doesn\'t displays expired projects but deliveries are shown', async () => {
+		const inTwoWeeks = moment().add(14, 'days').format()
+		mockdate.set(inTwoWeeks)
+		const projectsOnMarketplace = await getActiveProjects({ payload: {} })
+		const pendingDeliveries = await getPendingDeliveries({ payload: {} })
 
-  expect(projectsOnMarketplace.items.length).toEqual(0)
-  expect(pendingDeliveries.items.length).toEqual(1)
- })
- test('expired but approved deliveries are shown on my-projects', async () => {
-  await reviewDelivery({
-   payload: {
-    audit: projectDeliveredKey,
-    projectId: project.id,
-   },
-  })
-  const projectsOnMarketplace = await getActiveProjects({ payload: {} })
-  const pendingDeliveries = await getPendingDeliveries({ payload: {} })
-  const myProjects = await getMyProjects({ userId: 'user-differentuserid', payload: { currentPage: 1 } })
-  expect(projectsOnMarketplace.items.length).toEqual(0)
-  expect(pendingDeliveries.items.length).toEqual(0)
-  expect(myProjects.items.length).toEqual(1)
- })
+		expect(projectsOnMarketplace.items.length).toEqual(0)
+		expect(pendingDeliveries.items.length).toEqual(1)
+	})
+	test('expired but approved deliveries are shown on my-projects, but not approved are not', async () => {
+		mockdate.reset()
+		const project2 = await createProject({
+			userId: 'user-differentuserid',
+			payload: createProjectPayload(),
+		})
+		await auditProject({
+			userId: mockUserId,
+			payload: {
+				projectId: project2.id,
+				audit: projectApprovedKey,
+			},
+		})
+		const inTwoWeeks = moment().add(14, 'days').format()
+		mockdate.set(inTwoWeeks)
+		await reviewDelivery({
+			payload: {
+				audit: projectDeliveredKey,
+				projectId: project.id,
+			},
+		})
+		const projectsOnMarketplace = await getActiveProjects({ payload: {} })
+		const pendingDeliveries = await getPendingDeliveries({ payload: {} })
+		const myProjects = await getMyProjects({ userId: 'user-differentuserid', payload: { currentPage: 1 } })
+		expect(projectsOnMarketplace.items.length).toEqual(0)
+		expect(pendingDeliveries.items.length).toEqual(0)
+		expect(myProjects.items.length).toEqual(1)
+	})
 
- mockdate.reset()
+	mockdate.reset()
 })
