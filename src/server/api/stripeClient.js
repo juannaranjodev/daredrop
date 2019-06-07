53d0b427e6518d5c6c271c0614efe929d083d82c
Dominik Piekarski <piekarski_d@o2.pl>
2019-06-07 04:30:50 +0200

    fix

--- src/server/api/stripeClient.js
+++ src/server/api/stripeClient.js
@@ -11,8 +11,6 @@ export default new Promise((resolve, reject) => {
 				reject(err)
 			}
 			const { stripeSecret: clientSecret, stripeKey: clientId } = JSON.parse(data.SecretString)
-			console.log(clientSecret)
-			console.log(clientId)
 			const stripe = Stripe(clientSecret)
 			resolve(stripe)
 		})
