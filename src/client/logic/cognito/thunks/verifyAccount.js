b313185353d2144e3f1adb874c7d567aa9e24b75
Sebastian Corradini <sebastian34237@gmail.com>
2019-06-09 13:14:08 -0300

    Removed success verify apiRequest

--- src/client/logic/cognito/thunks/verifyAccount.js
+++ src/client/logic/cognito/thunks/verifyAccount.js
@@ -3,8 +3,6 @@ import { CognitoUser } from 'amazon-cognito-identity-js'
 import userPool from 'root/src/client/logic/cognito/util/userPool'
 import pushRoute from 'root/src/client/logic/route/thunks/pushRoute'
 import { LOGIN_ROUTE_ID } from 'root/src/shared/descriptions/routes/routeIds'
-import apiRequest from 'root/src/client/logic/api/thunks/apiRequest'
-import { SUCCESS_VERIFY } from 'root/src/shared/descriptions/endpoints/endpointIds'
 
 export default ({ email, verificationCode }) => dispatch => new Promise(
 	(resolve, reject) => {
@@ -19,12 +17,11 @@ export default ({ email, verificationCode }) => dispatch => new Promise(
 				resolve(result)
 			}
 		})
-	}
+	},
 ).then(
 	() => {
-		dispatch(apiRequest(SUCCESS_VERIFY, { email: email }))
 		dispatch(pushRoute(LOGIN_ROUTE_ID))
-	}
+	},
 ).catch(
-	console.warn
+	console.warn,
 )
--- src/server/api/googleClient.js
+++ src/server/api/googleClient.js
@@ -5,19 +5,19 @@ import { SecretsManager } from 'aws-sdk'
 const secretsClient = new SecretsManager()
 const secretName = 'google_OAuth'
 
-export default new Promise((resolve, reject) => {
-	secretsClient.getSecretValue({ SecretId: secretName }, (err, data) => {
-		if (err) {
-			reject(err)
-		}
-		const { clientSecret, refreshToken, clientId } = JSON.parse(data.SecretString)
-		const oauth2Client = new google.auth.OAuth2(clientId, clientSecret, redirectURI)
+// export default new Promise((resolve, reject) => {
+// 	secretsClient.getSecretValue({ SecretId: secretName }, (err, data) => {
+// 		if (err) {
+// 			reject(err)
+// 		}
+// 		const { clientSecret, refreshToken, clientId } = JSON.parse(data.SecretString)
+// 		const oauth2Client = new google.auth.OAuth2(clientId, clientSecret, redirectURI)
 
-		oauth2Client.setCredentials({
-			refresh_token: refreshToken,
-		})
-		resolve(oauth2Client)
-	})
-})
+// 		oauth2Client.setCredentials({
+// 			refresh_token: refreshToken,
+// 		})
+// 		resolve(oauth2Client)
+// 	})
+// })
 
-export const youtube = google.youtube('v3')
+// export const youtube = google.youtube('v3')
