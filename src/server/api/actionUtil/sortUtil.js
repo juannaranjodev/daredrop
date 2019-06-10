8b4e5118985e1af1ac13f5109aac5231ab723d37
Sebastian Corradini <sebastian34237@gmail.com>
2019-06-10 18:16:20 -0300

    Updated backend issue

--- src/server/api/actionUtil/sortUtil.js
+++ src/server/api/actionUtil/sortUtil.js
@@ -1,4 +1,4 @@
-import { ascend, descend, prop, propEq, add, reduce, propOr } from 'ramda'
+import { ascend, descend, prop, propEq, propOr } from 'ramda'
 import { SORT_BY_BOUNTY, SORT_BY_TIME_LEFT, SORT_BY_NEWEST, SORT_BY_CREATED_ASC, SORT_BY_ACCEPTED } from 'root/src/shared/constants/sortTypesOfProject'
 import { projectAccepted } from 'root/src/shared/descriptions/endpoints/recordTypes'
 
@@ -11,20 +11,9 @@ export const descendingPledgeAmount = descend(prop('pledgeAmount'))
 const isAcceptedDare = propEq('status', projectAccepted)
 
 const diffDescending = (a, b, prp) => {
-	const assigneesA = propOr([], 'assingees', a)
-	const assigneesB = propOr([], 'assignees', b)
-	const aSum = reduce(
-		(accum, itemA) => add(propOr(0, prp, itemA), accum),
-		0,
-		assigneesA,
-	)
-	const bSum = reduce(
-		(accum, itemB) => add(propOr(0, prp, itemB), accum),
-		0,
-		assigneesB,
-	)
-
-	return aSum - bSum
+	const amountA = propOr(0, prp, a)
+	const amountB = propOr(0, prp, b)
+	return amountB - amountA
 }
 
 const descendingAccepted = (a, b) => {
