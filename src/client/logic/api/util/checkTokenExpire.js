70d51ccdd1c3e717f61b10f941bc2e21224f162e
mazeit <coral1212@yahoo.com>
2019-06-11 18:56:52 -0400

    update refresh token when session is expired

--- src/client/logic/api/util/checkTokenExpire.js
+++ src/client/logic/api/util/checkTokenExpire.js
@@ -1,20 +1,11 @@
-import { prop, gte } from 'ramda'
-import jwtDecode from 'jwt-decode'
-import { SIGN_OUT, SIGN_IN } from 'root/src/shared/descriptions/routes/routeIds'
-import jwtTokenSelector from 'root/src/client/logic/auth/selectors/jwtTokenSelector'
-import pushRoute from 'root/src/client/logic/route/thunks/pushRoute'
+import determineAuth from 'root/src/client/logic/cognito/thunks/determineAuth'
+import { setRefreshSession, getCurrentSession } from 'root/src/client/logic/cognito/util/authenticate'
 
 export default async (state, dispatch) => {
-	const token = jwtTokenSelector(state)
-	if (!token)
-		return true
-	const decodedToken = jwtDecode(token)
-	const now = Math.ceil(Date.now() / 1000)
-	const expiryTime = prop('exp', decodedToken)
-	if (gte(now, expiryTime)) {
-		dispatch(pushRoute(SIGN_OUT, { }))
-		dispatch(pushRoute(SIGN_IN, { }))
-		return false
+	const session = await getCurrentSession()
+	if (!session.isValid()) {
+		await setRefreshSession()
+		await dispatch(determineAuth())		
 	}
 	return true
 }
