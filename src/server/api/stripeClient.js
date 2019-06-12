4b55fd327087b4670c4e08d7c074d42d6182f8fd
Sebastian Corradini <sebastian34237@gmail.com>
2019-06-11 21:45:48 -0300

    Backed clients

--- src/server/api/stripeClient.js
+++ src/server/api/stripeClient.js
@@ -4,17 +4,17 @@ import { SecretsManager } from 'aws-sdk'
 const secretsClient = new SecretsManager()
 const secretName = 'Stripe_Test_0'
 
-// export default new Promise((resolve, reject) => {
-// 	try {
-// 		secretsClient.getSecretValue({ SecretId: secretName }, (err, data) => {
-// 			if (err) {
-// 				reject(err)
-// 			}
-// 			const { stripeSecret: clientSecret, stripeKey: clientId } = JSON.parse(data.SecretString)
-// 			const stripe = Stripe(clientSecret)
-// 			resolve(stripe)
-// 		})
-// 	} catch (err) {
-// 		reject(err)
-// 	}
-// })
+export default new Promise((resolve, reject) => {
+	try {
+		secretsClient.getSecretValue({ SecretId: secretName }, (err, data) => {
+			if (err) {
+				reject(err)
+			}
+			const { stripeSecret: clientSecret, stripeKey: clientId } = JSON.parse(data.SecretString)
+			const stripe = Stripe(clientSecret)
+			resolve(stripe)
+		})
+	} catch (err) {
+		reject(err)
+	}
+})
