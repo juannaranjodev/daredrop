2a8dbc912a6c68a2fe66d639d30574a912dd746f
itsolution-git <itsolution0820@gmail.com>
2019-06-12 06:39:33 -0400

    bugfix/redirect-no-admin

--- src/client/logic/route/util/auditRoute.js
+++ src/client/logic/route/util/auditRoute.js
@@ -2,17 +2,18 @@ import { equals } from 'ramda'
 
 import routeDescriptions from 'root/src/shared/descriptions/routes'
 import isAuthenticated from 'root/src/client/logic/auth/selectors/isAuthenticated'
+import isAdminSelector from 'root/src/client/logic/auth/selectors/isAdminSelector'
 import {
 	authValue, unAuthValue, routeDescriptionLenses,
 } from 'root/src/client/logic/route/lenses'
 
 const { viewAuthentication } = routeDescriptionLenses
 export const auditRouteHof = (
-	authenticatedFn, routeDescriptionObj,
+	authenticatedFn, routeDescriptionObj
 ) => ({ routeId }, state) => {
-	const routeAuth = viewAuthentication(routeId, routeDescriptionObj)
+	const routeAuth = viewAuthentication(routeId, routeDescriptionObj)  
 	const authenticatedState = authenticatedFn(state) ? authValue : unAuthValue
-	return !routeAuth ? true : equals(routeAuth, authenticatedState)
+	return (isAdminSelector(state) || !routeAuth) ? true : equals(routeAuth, authenticatedState)
 }
 
 export default auditRouteHof(isAuthenticated, routeDescriptions)
