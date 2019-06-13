6f08476d5fd7d777bf6271e6548cc3e122a0e439
Dominik Piekarski <piekarski_d@o2.pl>
2019-06-13 17:29:36 +0200

    filled clientIds for client

--- src/shared/constants/app.js
+++ src/shared/constants/app.js
@@ -9,7 +9,7 @@ module.exports = (env) => {
 		case ('production'):
 			return {
 				TWITCH_CLIENT_ID: 'ruosppbybmeq0au48f4hzhzs0jfmej',
-				PAYPAL_CLIENT_ID: 'AZ7ruOMika_xOrNIVglKQcPUodUhuoe5ig4BDmZmVeZnWlm8dPCVenyrY7IZfyrT0ezOSDV_EtVwOPIe',
+				PAYPAL_CLIENT_ID: 'AUjfTQIkHBx0-WtUmsCTRk2_TgGcTH29xV9nxRZ09q1_ti9--64Orv7E47CsH5n_c_CER_qXN3woBjV9',
 				STRIPE_CLIENT_ID: 'pk_live_5YsMUBe3yGG1GL0gGDnPFyGe00kHLd5X7L',
 			}
 		default:
