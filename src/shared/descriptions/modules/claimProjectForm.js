a674548ea596e2237c98bfa786deac00b3d8a0ef
itsolution-git <itsolution0820@gmail.com>
2019-06-14 05:16:26 -0400

    bugfix/redirect-accept-success

--- src/shared/descriptions/modules/claimProjectForm.js
+++ src/shared/descriptions/modules/claimProjectForm.js
@@ -4,6 +4,9 @@ import { outlinedButton } from 'root/src/client/web/componentTypes'
 import {
 	CLAIM_PROJECT_FORM_MODULE_ID,
 } from 'root/src/shared/descriptions/modules/moduleIds'
+import {
+	DARE_ACCEPT_SUCCESS_ROUTE_ID,
+} from 'root/src/shared/descriptions/routes/routeIds'
 
 import { ACCEPT_PROJECT } from 'root/src/shared/descriptions/endpoints/endpointIds'
 import claimProjectSchemaSelector from 'root/src/shared/util/claimProjectSchemaSelector'
@@ -37,10 +40,17 @@ export default {
 				subFieldTopText: 'This is the amount your fans need to pledge in order for you to deliver. Only about 90% of charges are generally successful, take that into account when setting your goal.',
 			},
 		],
+		successPage: DARE_ACCEPT_SUCCESS_ROUTE_ID,
 		submits: [
 			{
 				label: 'Accept',
 				endpointId: ACCEPT_PROJECT,
+				onSuccessRedirect: {
+					routeId: DARE_ACCEPT_SUCCESS_ROUTE_ID,
+					routeParams: [
+						['recordId', { $sub: ['res', 'body', 'id'] }],
+					],
+				},
 			},
 		],
 		handlers: [
