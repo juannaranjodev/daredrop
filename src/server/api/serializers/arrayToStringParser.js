7161abedc90ddb9fa61639749f929d10eea64d10
jbechtold <joebechtold@gmail.com>
2019-06-20 11:29:13 -0400

    changed or to and

--- src/server/api/serializers/arrayToStringParser.js
+++ src/server/api/serializers/arrayToStringParser.js
@@ -2,6 +2,6 @@ import { join, length, equals, lte, slice, last } from 'ramda'
 
 export default (array) => {
 	if (lte(length(array), 1)) return join('', array)
-	if (equals(length(array), 2)) return join(' or ', array)
-	return `${join(', ', slice(0, -1, array))}, or ${last(array)}`
+	if (equals(length(array), 2)) return join(' and ', array)
+	return `${join(', ', slice(0, -1, array))}, and ${last(array)}`
 }
