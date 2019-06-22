8349863d76840fa75849abd28312a9d51535a15e
itsolution-git <itsolution0820@gmail.com>
2019-06-22 18:05:55 -0400

    update api

--- src/server/api/actions/getAcceptProject.js
+++ src/server/api/actions/getAcceptProject.js
@@ -1,19 +1,6 @@
-import { reduce, prop } from 'ramda'
+import { map, prop, assoc, pick } from 'ramda'
 import { projectAcceptedKey } from 'root/src/server/api/lenses'
 import getProjectsByStatus from 'root/src/server/api/actionUtil/getProjectsByStatus'
 import { SORT_BY_NEWEST } from 'root/src/shared/constants/sortTypesOfProject'
 
-export default async (payload) => getProjectsByStatus(projectAcceptedKey, SORT_BY_NEWEST, payload, false, false, false)
-	.then(projects => {
-		const items = reduce(
-			(result, project) => {
-				if (project) {
-					return [...result, { id: project.id, accepted: project.created }]
-				}
-				return result
-			},
-			[],
-			prop('items', projects),
-		)		
-		return { items }		
-	})
+export default async payload => assoc('items', map(pick(['id']), prop('items', await getProjectsByStatus(projectAcceptedKey, SORT_BY_NEWEST, payload, false, false, false, true))), {})
