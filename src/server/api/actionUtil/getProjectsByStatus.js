020e9311fb9d6047b65c4b14bb2a1f0cf8c44846
Dominik Piekarski <piekarski_d@o2.pl>
2019-06-26 20:07:30 +0200

    accepted projects no pagination

--- src/server/api/actionUtil/getProjectsByStatus.js
+++ src/server/api/actionUtil/getProjectsByStatus.js
@@ -12,11 +12,10 @@ import getFilteredProjectIds from 'root/src/server/api/actionUtil/getFilteredPro
 
 const PageItemLength = 8
 
-export default async (status, defaultSortType, payload, isAdminEndpoint, noExpirationFilter, isDenormalized, noPagination=false) => {
+export default async (status, defaultSortType, payload, isAdminEndpoint, noExpirationFilter, isDenormalized, noPagination) => {
 	const realPayload = payload.payload
 	const projectsDdb = await dynamoQueryShardedProjects(status, isDenormalized)
 	const serializedProjects = map(compose(dissoc('myPledge'), projectSerializer(__, isAdminEndpoint, isDenormalized)), projectsDdb)
-
 	// Filter expired projects
 	const filterExpired = (dare) => {
 		const diff = moment().diff(dare.approved, 'days')
