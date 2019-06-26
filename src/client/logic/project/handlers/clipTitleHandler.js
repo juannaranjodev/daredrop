03e1c1528f0568e569c10ebca57994eace8ad300
AnnaBova <anna.bova23@gmail.com>
2019-06-26 11:31:18 +0300

    fix clip header

--- src/client/logic/project/handlers/clipTitleHandler.js
+++ src/client/logic/project/handlers/clipTitleHandler.js
@@ -1,8 +1,8 @@
 import { slice } from 'ramda'
 
 export default (title) => {
-	if (title.length > 42) {
-		const newTitle = `${slice(0, 42, title)}...`
+	if (title.length > 32) {
+		const newTitle = `${slice(0, 32, title)}...`
 		return newTitle
 	}
 	return title
