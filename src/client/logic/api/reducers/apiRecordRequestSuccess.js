fb8a603af6075a6647a6882b98a873f60a9127e2
Dominik Piekarski <piekarski_d@o2.pl>
2019-06-30 14:13:56 +0200

    wip

--- src/client/logic/api/reducers/apiRecordRequestSuccess.js
+++ src/client/logic/api/reducers/apiRecordRequestSuccess.js
@@ -3,8 +3,9 @@ import { compose } from 'ramda'
 import { API_RECORD_REQUEST_SUCCESS } from 'root/src/client/logic/api/actionIds'
 import createRecordStoreKey from 'root/src/client/logic/api/util/createRecordStoreKey'
 import { apiStoreLenses, idProp } from 'root/src/client/logic/api/lenses'
+import getLensFromType from 'root/src/client/logic/api/util/getLensFromType'
 
-const { setRecordsChild, setRecordProcessingChild } = apiStoreLenses
+const { setRecordProcessingChild } = apiStoreLenses
 
 export const apiRecordRequestSuccess = (
 	state,
@@ -12,6 +13,7 @@ export const apiRecordRequestSuccess = (
 ) => {
 	const recordId = idProp(record)
 	const recordStoreId = createRecordStoreKey(recordType, recordId)
+	const setRecordsChild = getLensFromType('set', 'child', recordType, apiStoreLenses)
 	return compose(
 		setRecordsChild(recordStoreId, record),
 		setRecordProcessingChild(recordStoreId, false),
