d3386006de6f500bfeaf99d171984a6f97a46fd5
Sebastian Corradini <sebastian34237@gmail.com>
2019-06-30 14:37:42 -0300

    Fixed Claim button

--- src/client/logic/project/selectors/projectAcceptanceStatusSelector.js
+++ src/client/logic/project/selectors/projectAcceptanceStatusSelector.js
@@ -1,4 +1,4 @@
-import { filter, length, map, prop, intersection, gt, propEq, head, isNil, equals } from 'ramda'
+import { filter, length, map, prop, intersection, gt, propEq, head, hasPath } from 'ramda'
 import objectToArray from 'root/src/client/logic/api/util/objectToArray'
 
 import projectAssigneesSelector from 'root/src/client/logic/project/selectors/projectAssigneesSelector'
@@ -9,8 +9,6 @@ import {
 	streamerAcceptedKey,
 	streamerRejectedKey,
 	streamerDeliveredKey,
-	streamerDeliveryApprovedKey,
-	streamerDeliveryRejectedKey,
 } from 'root/src/server/api/lenses'
 
 export default (state, props) => {
@@ -20,10 +18,7 @@ export default (state, props) => {
 	const assigneesDisplayNames = map(prop('displayName'), assignees)
 	const assigneeNames = intersection(userDataDisplayNames, assigneesDisplayNames)
 	const isAssignee = gt(length(assigneeNames), 0)
-	console.log('assignees', assignees, 'userData', userData, 'userDataDisplayNames', userDataDisplayNames, 'assigneesDisplayNames', assigneesDisplayNames, 'assigneeNames', assigneeNames, 'isAssignee', isAssignee)
-	const matches = head(assigneeNames)
-	const assigneObject = head(filter(assigne => equals(prop('displayName', assigne), matches), assignees))
-	const deliveryVideoStatus = prop('deliveryVideo', assigneObject)
+	const deliveryVideos = filter(hasPath(['deliveryVideo']), assignees)
 
 	if (!isAssignee) {
 		return notConnected
@@ -33,10 +28,8 @@ export default (state, props) => {
 	const assignee = head(filteredAssignee)
 	const acceptanceStatus = prop('accepted', assignee)
 	if (
-		!isNil(deliveryVideoStatus)
-		&& (
-			acceptanceStatus !== streamerDeliveryApprovedKey
-			&& acceptanceStatus !== streamerDeliveryRejectedKey)
+		gt(length(deliveryVideos), 0)
+		&& acceptanceStatus === streamerAcceptedKey
 	) {
 		return videoPendingAproved
 	}
