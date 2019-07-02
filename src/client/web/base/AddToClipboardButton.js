a32cbb9fa1c2b53ab5520792beb7b217b0164391
AnnaBova <anna.bova23@gmail.com>
2019-07-02 16:01:17 +0300

    add copied message

--- src/client/web/base/AddToClipboardButton.js
+++ src/client/web/base/AddToClipboardButton.js
@@ -1,20 +1,40 @@
 import React, { Component } from 'react'
 import copy from 'copy-to-clipboard'
+import { primaryColor } from 'root/src/client/web/commonStyles'
 
 class AddToClipboard extends Component {
 	constructor(props) {
 		super(props)
 		this.text = React.createRef()
+		this.state = {
+			isCopied: false,
+		}
 	}
 
 	addToClipboard = () => {
 		const { url } = this.props
 		copy(url)
+		this.setState({ isCopied: true })
 	}
 
 	render() {
+		const { isCopied } = this.state
 		return (
 			<div>
+				{ isCopied && (
+					<div style={{
+						position: 'absolute',
+						right: 8,
+						top: -11,
+						color: '#ffffff',
+						fontWeight: 'bold',
+						backgroundColor: primaryColor,
+						padding: '0px 12px',
+						borderRadius: 5,
+					}}
+					>Copied!
+					</div>
+				) }
 				<div onClick={this.addToClipboard}>{this.props.children}</div>
 			</div>
 		)
