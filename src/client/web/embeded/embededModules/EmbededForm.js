b1cd732aa2c308c47418617d9cb44ba92aeb6e86
Dominik Piekarski <piekarski_d@o2.pl>
2019-07-03 00:31:14 +0200

    fixed bug

--- src/client/web/embeded/embededModules/EmbededForm.js
+++ src/client/web/embeded/embededModules/EmbededForm.js
@@ -2,7 +2,5 @@ import React, { memo } from 'react'
 import EmbededFields from 'root/src/client/web/embeded/embededModules/EmbededFields'
 
 export default memo(({ classes, ...props }) => (
-	<div className={classes.embededFormModule}>
-		<EmbededFields {...props} />
-	</div>
+	<EmbededFields {...props} />
 ))
