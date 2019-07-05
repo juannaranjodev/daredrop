93f975f25da9e6c66c6e1f64fb9b2c5f0b1203cc
AnnaBova <anna.bova23@gmail.com>
2019-07-05 10:22:18 +0300

    fix email template

--- src/server/email/templates/payoutFailure.js
+++ src/server/email/templates/payoutFailure.js
@@ -3,7 +3,7 @@ import { ourName, paypalEmailUrl } from 'root/src/shared/constants/mail'
 
 export default () => {
 	const mailContent = `
-              <table border="0" cellpadding="0" cellspacing="0" style="margin-top:0;margin-bottom:0;margin-left:auto;margin-right:auto;padding:0;color:#354052;font-family:Roboto,sans-serif;font-size:26px;line-height:1.25;" width="480" class="content">
+              <table border="0" cellpadding="0" cellspacing="0" style="margin-top:0;margin-bottom:0;margin-left:auto;margin-right:auto;padding:0;color:#354052;font-family:Roboto,sans-serif;font-size:26px;line-height:1.25;" width="80%" class="content">
                 <tbody>
                   <tr>
                     <td height="50" style="margin:0;padding:0;" width="100%"></td>
