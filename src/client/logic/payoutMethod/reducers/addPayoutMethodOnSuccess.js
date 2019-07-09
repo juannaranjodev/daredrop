316a37f497fbb0009ab237ea900acded9a49ca59
AnnaBova <anna.bova23@gmail.com>
2019-07-09 10:18:05 +0300

    fix payout method

--- src/client/logic/payoutMethod/reducers/addPayoutMethodOnSuccess.js
+++ src/client/logic/payoutMethod/reducers/addPayoutMethodOnSuccess.js
@@ -1,14 +1,8 @@
 import { ADD_PAYOUT_METHOD_ON_SUCCESS } from 'root/src/client/logic/payoutMethod/actionIds'
-import createPayoutMethodStoreKey from 'root/src/client/logic/payoutMethod/util/createPayoutMethodStoreId'
-import currentRouteParamsRecordId from 'root/src/client/logic/route/selectors/currentRouteParamsRecordId'
 import { apiStoreLenses } from 'root/src/client/logic/api/lenses'
 
-const { setPayoutMethodChild } = apiStoreLenses
+const { setPayoutMethod } = apiStoreLenses
 
 export default {
-	[ADD_PAYOUT_METHOD_ON_SUCCESS]: (state, { body }) => {
-		const paramsRecordId = currentRouteParamsRecordId(state)
-		const paymentMethodStoreKey = createPayoutMethodStoreKey(paramsRecordId)
-		return setPayoutMethodChild(paymentMethodStoreKey, body, state)
-	},
+	[ADD_PAYOUT_METHOD_ON_SUCCESS]: (state, { body }) => setPayoutMethod(body, state),
 }
