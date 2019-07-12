478572c172e273da875c5ead3bd23e2c3d3a8cbc
Sebastian Corradini <sebastian34237@gmail.com>
2019-07-12 12:37:17 -0300

    Fixed some issues in DeliveryDareInit and DeliveryDare for SES

--- src/server/api/actions/deliveryDare.js
+++ src/server/api/actions/deliveryDare.js
@@ -18,6 +18,7 @@ import getUserEmail from 'root/src/server/api/actionUtil/getUserEmail'
 import { videoSubmittedTitle } from 'root/src/server/email/util/emailTitles'
 import videoSubmittedEmail from 'root/src/server/email/templates/videoSubmitted'
 import sendEmail from 'root/src/server/email/actions/sendEmail'
+import projectHrefBuilder from 'root/src/server/api/actionUtil/projectHrefBuilder'
 
 const payloadLenses = getPayloadLenses(DELIVERY_DARE)
 const { viewDeliverySortKey, viewProjectId, viewTestName } = payloadLenses
