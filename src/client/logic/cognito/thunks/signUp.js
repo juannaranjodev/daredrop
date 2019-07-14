bde7cf92c148f709c60880986659d7eda9851b5d
Sebastian Corradini <sebastian34237@gmail.com>
2019-07-14 12:40:42 -0300

    Added regex

--- src/client/logic/cognito/thunks/signUp.js
+++ src/client/logic/cognito/thunks/signUp.js
@@ -6,30 +6,29 @@ import { emailRe } from 'root/src/shared/util/regexes'
 
 export default ({ email, password }) => dispatch => new Promise(
 	(resolve, reject) => {
-		console.log(test(emailRe, email))
-
-		if (1) {
+		if (!test(emailRe, email)) {
 			const fieldError = {
 				email: 'Please input correct email address.',
 			}
 			reject(fieldError)
-		}
-		userPool.signUp(
-			email, password, [], null, (cognitoError, result) => {
-				if (cognitoError) {
-					const { code, message } = cognitoError
-					let fieldError = { email: message }
-					if (code === 'InvalidPasswordException') {
-						fieldError = {
-							password: 'Password must contain at least one number, symbol, and uppercase character',
+		} else {
+			userPool.signUp(
+				email, password, [], null, (cognitoError, result) => {
+					if (cognitoError) {
+						const { code, message } = cognitoError
+						let fieldError = { email: message }
+						if (code === 'InvalidPasswordException') {
+							fieldError = {
+								password: 'Password must contain at least one number, symbol, and uppercase character',
+							}
 						}
+						reject(fieldError)
+					} else {
+						resolve(result.user)
 					}
-					reject(fieldError)
-				} else {
-					resolve(result.user)
-				}
-			},
-		)
+				},
+			)
+		}
 	},
 ).then(
 	() => dispatch(pushRoute(VERIFY_ACCOUNT_ROUTE_ID)),
