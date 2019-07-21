1c6894ec7601ae7a640f47dd846277ae43c7fe09
Dominik Piekarski <piekarski_d@o2.pl>
2019-07-21 03:03:55 +0200

    removed console log

--- src/server/api/index.js
+++ src/server/api/index.js
@@ -66,7 +66,6 @@ export const apiHof = (
 
 		return { statusCode: 200, body: res }
 	} catch (error) {
-		console.log(JSON.stringify(error, null, 2))
 		const errorMessage = error.message
 		return customError(error.statusCode || 500, {
 			...(errorMessage ? { generalErrors: errorMessage } : {}),
