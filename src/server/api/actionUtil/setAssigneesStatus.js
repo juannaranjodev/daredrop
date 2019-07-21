cd7cf5fa977430bf29c23a417d32050774c15a47
Dominik Piekarski <piekarski_d@o2.pl>
2019-07-21 23:04:41 +0200

    fixed bug with project status

--- src/server/api/actionUtil/setAssigneesStatus.js
+++ src/server/api/actionUtil/setAssigneesStatus.js
@@ -1,7 +1,7 @@
 import { map, prop, omit, contains } from 'ramda'
 
 export default (project, assigneeStatus, userTokensStr, amountRequested) => ({
-	...omit(['id'], project),
+	...omit(['status', 'id'], project),
 	assignees: map((assignee) => {
 		const assigneeId = `${assignee.platform}|${assignee.platformId}`
 		if (contains(assigneeId, userTokensStr)) {
