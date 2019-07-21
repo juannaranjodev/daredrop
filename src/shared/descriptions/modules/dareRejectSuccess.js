620e8916be38765b0edf8ba5dcdf2856296a73c8
jbechtold <joebechtold@gmail.com>
2019-07-21 18:18:51 -0400

    improved quality of dare reject image

--- src/shared/descriptions/modules/dareRejectSuccess.js
+++ src/shared/descriptions/modules/dareRejectSuccess.js
@@ -1,7 +1,7 @@
 import {
 	REJECT_DARE_SUCCESS_MODULE_ID,
 } from 'root/src/shared/descriptions/modules/moduleIds'
-import bannerFooterImage from 'root/src/client/assets/Dare-reject-confirmation-page.jpg'
+import bannerFooterImage from 'root/src/client/assets/Dare-reject-confirmation-page1.jpg'
 import { ACTIVE_PROJECTS_ROUTE_ID } from 'root/src/shared/descriptions/routes/routeIds'
 
 export default {
