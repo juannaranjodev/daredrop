9fe2995b5b222d6c2c299ec0fd27600132f1b963
Dominik Piekarski <piekarski_d@o2.pl>
2019-07-23 21:50:02 +0200

    added loader to lambda webpackk

--- src/aws/util/cfCli/webpackLambdaConf.js
+++ src/aws/util/cfCli/webpackLambdaConf.js
@@ -42,6 +42,13 @@ module.exports = {
 					name: '[name].[ext]',
 				},
 			},
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'webpack-conditional-loader',
+				},
+			},
 		],
 	},
 	plugins: [
